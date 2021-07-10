de2d2404cd88d72d7a02e75e766d1c500ad8e215
ellttBen <elliott@bonfida.com>
2021-07-10 12:19:27 +0100

    Revert findActiveAuctionsForUSer

--- js/src/secondary_bindings.ts
+++ js/src/secondary_bindings.ts
@@ -13,12 +13,7 @@ import { AUCTION_PROGRAM_ID, PROGRAM_ID } from './bindings';
 export async function findActiveAuctionsForUser(
   connection: Connection,
   userAccount: PublicKey
-): Promise<
-  {
-    publicKey: PublicKey;
-    accountInfo: AccountInfo<Buffer>;
-  }[]
-> {
+) {
   const filters = [
     {
       memcmp: {
@@ -27,11 +22,14 @@ export async function findActiveAuctionsForUser(
       },
     },
   ];
-  return await getFilteredProgramAccounts(
+  const accounts = await getFilteredProgramAccounts(
     connection,
     AUCTION_PROGRAM_ID,
     filters
   );
+  return accounts.map((a) => {
+    return new PublicKey(a.accountInfo.data.slice(64, 96));
+  });
 }
 
 export async function findEndingAuctions(
